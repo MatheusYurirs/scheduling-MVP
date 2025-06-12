70581575d28a9f52591f2ea98a7948d1ab7eee16
Matheus Yuri Silva <matheusyurirs@gmail.com>
2025-06-12 15:58:27 -0300

    first commit

---
+++ src/main/java/br/com/yuricodev/schedulingmvp/SchedulingmvpApplication.java
@@ -0,0 +1,13 @@
+package br.com.yuricodev.schedulingmvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulingmvpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulingmvpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/yuricodev/schedulingmvp/SchedulingmvpApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.yuricodev.schedulingmvp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulingmvpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
