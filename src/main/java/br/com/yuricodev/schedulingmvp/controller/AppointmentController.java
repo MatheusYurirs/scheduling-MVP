32000938787c1ff7cf344ee008407c619842e0cd
Matheus Yuri Silva <matheusyurirs@gmail.com>
2025-06-13 19:24:44 -0300

    feat: implementa o controller e o service do Appointment

--- src/main/java/br/com/yuricodev/schedulingmvp/controller/AppointmentController.java
+++ src/main/java/br/com/yuricodev/schedulingmvp/controller/AppointmentController.java
@@ -1,4 +1,39 @@
 package br.com.yuricodev.schedulingmvp.controller;
 
+import br.com.yuricodev.schedulingmvp.entity.Appointment;
+import br.com.yuricodev.schedulingmvp.service.AppointmentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/appointment")
 public class AppointmentController {
+
+    private final AppointmentService appointmentService;
+
+    public AppointmentController(AppointmentService appointmentService) {
+        this.appointmentService = appointmentService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Appointment> createAppointment(@RequestBody Appointment appointment){
+        Appointment create = appointmentService.createAppointment(appointment);
+        return ResponseEntity.status(HttpStatus.CREATED).body(create);
+    }
+
+    @GetMapping("/getAppointments")
+    public ResponseEntity<List<Appointment>> getAllAppointments(){
+        List<Appointment> appointments = appointmentService.getAppointmnet();
+        return ResponseEntity.ok(appointments);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteAppointment(@PathVariable Long id){
+         appointmentService.deleteAppointment(id);
+        return ResponseEntity.noContent().build();
+
+    }
 }
--- src/main/java/br/com/yuricodev/schedulingmvp/service/AppointmentService.java
+++ src/main/java/br/com/yuricodev/schedulingmvp/service/AppointmentService.java
@@ -1,4 +1,58 @@
 package br.com.yuricodev.schedulingmvp.service;
 
+import br.com.yuricodev.schedulingmvp.entity.Appointment;
+import br.com.yuricodev.schedulingmvp.entity.Enums.TypeStatus;
+import br.com.yuricodev.schedulingmvp.repository.AppointmentRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class AppointmentService {
+
+    private final AppointmentRepository appointmentRepository;
+    private final AvailableSlotService availableSlotService;
+
+
+    public AppointmentService(AppointmentRepository appointmentRepository, AvailableSlotService availableSlotService) {
+        this.appointmentRepository = appointmentRepository;
+        this.availableSlotService = availableSlotService;
+    }
+
+    public Appointment createAppointment(Appointment request){
+        if(!availableSlotService.isSlotAvailable(request.getDateTime())){
+            throw new RuntimeException("Horário já ocupado");
+        }
+
+        Appointment appointment = new Appointment();
+        appointment.setDateTime(request.getDateTime());
+        appointment.setClientName(request.getClientName());
+        if(request.getStatus() != null) {
+            appointment.setStatus(request.getStatus());
+        }
+        else {
+            appointment.setStatus(TypeStatus.AGENDADO);
+        }
+
+        Appointment saveAppointment = appointmentRepository.save(appointment);
+
+        availableSlotService.markSlotAsTaken(request.getDateTime());
+
+        return saveAppointment;
+    }
+
+    public List<Appointment> getAppointmnet(){
+        List<Appointment> appointments = appointmentRepository.findAll();
+        return appointments;
+    }
+
+    public void deleteAppointment(Long id) {
+        Appointment appointment = appointmentRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Agendamento não encontrado"));
+
+        availableSlotService.markSlotAsFree(appointment.getDateTime());
+        appointmentRepository.deleteById(id);
+    }
+
+
 }
