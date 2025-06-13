a6ece9a0662fa9a64e4db3e210c29e773618bf23
Matheus Yuri Silva <matheusyurirs@gmail.com>
2025-06-13 17:59:24 -0300

    feat: implementa o Controller e o Service de AvailableSlotService

--- src/main/java/br/com/yuricodev/schedulingmvp/controller/AvailableSlotController.java
+++ src/main/java/br/com/yuricodev/schedulingmvp/controller/AvailableSlotController.java
@@ -1,4 +1,46 @@
 package br.com.yuricodev.schedulingmvp.controller;
 
+import br.com.yuricodev.schedulingmvp.entity.AvailableSlot;
+import br.com.yuricodev.schedulingmvp.service.AvailableSlotService;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/slots")
 public class AvailableSlotController {
+
+    private final AvailableSlotService availableSlotService;
+
+    public AvailableSlotController(AvailableSlotService availableSlotService) {
+        this.availableSlotService = availableSlotService;
+    }
+
+    @GetMapping("/available")
+    public ResponseEntity<List<AvailableSlot>> getAvailable(@RequestParam("date")
+                                                            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
+        List<AvailableSlot> availableSlots = availableSlotService.getAvailableSlots(date);
+        return ResponseEntity.ok(availableSlots);
+    }
+
+    @GetMapping("/check")
+    public ResponseEntity<Boolean> isSlotAvailabe(@RequestParam("datetime")
+                                                  @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dateTime
+    ) {
+        boolean available = availableSlotService.isSlotAvailable(dateTime);
+        return ResponseEntity.ok(available);
+    }
+
+    @PostMapping
+    public ResponseEntity<AvailableSlot> createSlot(@RequestBody AvailableSlot availableSlot){
+        AvailableSlot slot = availableSlotService.createSlot(availableSlot.getDateTime());
+        return ResponseEntity.status(HttpStatus.CREATED).body(slot);
+    }
+
+
 }
--- src/main/java/br/com/yuricodev/schedulingmvp/service/AvailableSlotService.java
+++ src/main/java/br/com/yuricodev/schedulingmvp/service/AvailableSlotService.java
@@ -1,4 +1,66 @@
 package br.com.yuricodev.schedulingmvp.service;
 
+import br.com.yuricodev.schedulingmvp.entity.AvailableSlot;
+import br.com.yuricodev.schedulingmvp.repository.AvailableSlotRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
 public class AvailableSlotService {
+
+    private final AvailableSlotRepository availableSlotRepository;
+
+    public AvailableSlotService(AvailableSlotRepository availableSlotRepository) {
+        this.availableSlotRepository = availableSlotRepository;
+    }
+
+    public List<AvailableSlot> getAvailableSlots(LocalDate date){
+        LocalDateTime start = date.atStartOfDay();
+        LocalDateTime end = date.plusDays(1).atStartOfDay();
+        return availableSlotRepository.findAllByIsTakenFalseAndDateTimeBetween(start, end);
+    }
+
+    public AvailableSlot createSlot(LocalDateTime dateTime){
+        if (availableSlotRepository.findByDateTime(dateTime).isPresent()) {
+            throw new RuntimeException("Esse horário já existe.");
+        }
+
+        AvailableSlot slot = new AvailableSlot();
+        slot.setDateTime(dateTime);
+        slot.setTaken(false);
+        return availableSlotRepository.save(slot);
+    }
+
+    public void cancelSlot(Long id){
+        availableSlotRepository.deleteById(id);
+    }
+
+    public boolean isSlotAvailable(LocalDateTime dateTime){
+        return availableSlotRepository.findByDateTime(dateTime)
+                .map(slot -> !slot.isTaken())
+                .orElse(false);
+    }
+
+    public void markSlotAsTaken(LocalDateTime dateTime) {
+        AvailableSlot slot = availableSlotRepository.findByDateTime(dateTime)
+                .orElseThrow(() -> new RuntimeException("Slot não encontrado"));
+
+        if(slot.isTaken()){
+            throw new RuntimeException("Esté horário já está ocupado.");
+        }
+
+        slot.setTaken(true);
+        availableSlotRepository.save(slot);
+    }
+
+    public void markSlotAsFree(LocalDateTime dateTime) {
+        AvailableSlot slot = availableSlotRepository.findByDateTime(dateTime)
+                .orElseThrow(() -> new RuntimeException("Slot não encontrado"));
+
+        slot.setTaken(false);
+        availableSlotRepository.save(slot);
+    }
 }
