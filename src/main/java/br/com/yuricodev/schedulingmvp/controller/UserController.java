6a1940a2172d9e9c6453dd3ee50580e3cb9bb702
Matheus Yuri Silva <matheusyurirs@gmail.com>
2025-06-15 11:24:24 -0300

    feat: implementa a service e controller do usuario

--- src/main/java/br/com/yuricodev/schedulingmvp/controller/UserController.java
+++ src/main/java/br/com/yuricodev/schedulingmvp/controller/UserController.java
@@ -1,4 +1,47 @@
 package br.com.yuricodev.schedulingmvp.controller;
 
+import br.com.yuricodev.schedulingmvp.entity.User;
+import br.com.yuricodev.schedulingmvp.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(User user){
+        userService.createUser(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers(){
+        List<User> users = userService.getAllUsers();
+        return ResponseEntity.ok(users);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id){
+       return userService.findById(id)
+               .map(ResponseEntity::ok)
+               .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteBytId(@PathVariable Long id){
+        userService.deleteById(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
 }
--- src/main/java/br/com/yuricodev/schedulingmvp/service/UserService.java
+++ src/main/java/br/com/yuricodev/schedulingmvp/service/UserService.java
@@ -1,4 +1,38 @@
 package br.com.yuricodev.schedulingmvp.service;
 
+import br.com.yuricodev.schedulingmvp.entity.User;
+import br.com.yuricodev.schedulingmvp.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User createUser(User user){
+        User createUser = userRepository.save(user);
+        return createUser;
+    }
+
+    public List<User> getAllUsers(){
+        List<User> users = userRepository.findAll();
+        return users;
+    }
+
+    public Optional<User> findById(Long id){
+        return userRepository.findById(id);
+    }
+
+    public void deleteById(Long id){
+        User user =userRepository.findById(id).orElseThrow(() -> new RuntimeException("Usuario com o id "+ id +" não encontrado"));
+        userRepository.deleteById(id);
+    }
+
 }
